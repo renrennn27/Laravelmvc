770f791ddbd3c83b846cd618d12546fcf99704de
renrennn27 <renoalifprasetyo20@gmail.com>
2025-10-27 11:19:29 +0700

    DB seeder and faker

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
-    //
+     use HasFactory;
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
 }
